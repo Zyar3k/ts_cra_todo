152b71b910c71985db00354d3bb82a82746be2ce
zyar3k <a.zygan@gmail.com>
2022-02-21 13:27:16 +0100

    Add todo to array

--- src/components/InputFiled.tsx
+++ src/components/InputFiled.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import "./styles.css";
 
-const InputFiled = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputFiled: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
   return (
-    <form className="input">
+    <form className="input" onSubmit={handleAdd}>
       <input
         type="input"
         placeholder="Wpisz zadanie..."
         className="input__box"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
       />
       <button className="input__submit" type="submit">
         Dodaj
