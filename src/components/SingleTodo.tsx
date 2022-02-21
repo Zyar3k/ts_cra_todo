46e9e6ea3f9f9f1cc11fe402e3d9fb7bc310b063
zyar3k <a.zygan@gmail.com>
2022-02-21 14:19:07 +0100

    Add delete and done func

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -11,17 +11,35 @@ type Props = {
 };
 
 const SingleTodo = ({ todo, setTodos, todos }: Props) => {
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const handleEdit = (id: number) => {};
+
   return (
     <form className="todos__single">
-      <span className="todos__single--text">{todo.todo}</span>
+      {todo.isDone ? (
+        <s className="todos__single--text">{todo.todo}</s>
+      ) : (
+        <span className="todos__single--text">{todo.todo}</span>
+      )}
       <div>
-        <span className="icon">
+        <span className="icon" onClick={() => handleEdit(todo.id)}>
           <AiFillEdit />
         </span>
-        <span className="icon">
+        <span className="icon" onClick={() => handleDelete(todo.id)}>
           <AiFillDelete />
         </span>
-        <span className="icon">
+        <span className="icon" onClick={() => handleDone(todo.id)}>
           <MdDone />
         </span>
       </div>
