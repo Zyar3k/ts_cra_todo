e1b2c7e453f8db78757eb1ffb212e32c588e9ff1
zyar3k <a.zygan@gmail.com>
2022-02-21 12:02:04 +0100

    TS basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+let name: string;
+let age: number | string;
+let isStudent: boolean;
+let hobbies: string[];
+let role: [number, string];
+
+let printName: (name: string) => number;
+
+// function printName(name:string){
+//     console.log(name)
+// }
+// printName('John')
+
+// type Person = {
+//     name: string
+//     age?: number
+// }
+// let person: Person={
+//     name: 'Max',
+// }
+// let lotsOfPeople: Person[]
+
+type X = {
+  a: string;
+  b: number;
+};
+interface Person extends X {
+  name: string;
+  age?: number;
+}
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  return <div className="App">Hello World</div>;
+};
 
 export default App;
